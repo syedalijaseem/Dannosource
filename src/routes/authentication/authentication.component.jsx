8ceb9579df3ef89053c99d5a0d2f29efb2b18ac5
syedalijaseem <syedali0057@gmail.com>
2023-02-08 22:48:54 +0500

    Updated authentication, added SignIn feature and styling

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -5,14 +5,15 @@ import { getRedirectResult } from "firebase/auth";
 
 import {
   auth,
-  signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils.js";
 
 import SignUpFrom from "../../components/sign-up-form/sign-up-form.component.jsx";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component.jsx";
 
-const SignIn = () => {
+import "./authentication.styles.scss";
+
+const Authentication = () => {
   useEffect(async () => {
     const response = await getRedirectResult(auth);
     console.log(response);
@@ -22,21 +23,12 @@ const SignIn = () => {
     }
   }, []);
 
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
+    <div className="authentication-container">
+      <SignInForm />
       <SignUpFrom />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
